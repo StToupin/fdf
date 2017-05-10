7ae3f3f6caba51cfdf55162e14b7fbbd2e25cdb3
Steven TOUPIN <stoupin@e2r7p7.42.fr>
2017-05-10 14:15:08 +0200

    Adding test maps

--- main.c
+++ main.c
@@ -1,31 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include "get_next_line.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stoupin <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/09 14:52:22 by stoupin           #+#    #+#             */
+/*   Updated: 2017/05/09 14:52:23 by stoupin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int main(int argc, char **argv)
+#include <unistd.h>
+#include "fdf.h"
+
+int	main(int argc, char **argv)
 {
-	char	*line;
-	int		fd;
-	int		i;
+	t_env	env;
+	int		err;
 
-	if (argc != 2)
-	{
-		printf("Usage: %s filename\n", argv[0]);
-		return (0);
-	}
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
-	{
-		printf("Error opening '%s'.\n", argv[1]);
-		return (0);
-	}
-	i = 0;
-	while (get_next_line(fd, &line) == 1)
-	{
-		printf("%03d %s\n", i + 1, line);
-		free(line);
-		i++;
-	}
+	(void)argc;
+	(void)argv;
+	err = init(&env);
+	if (err)
+		write(1, "Error\n", 6);
+	die(&env);
 	return (0);
 }
