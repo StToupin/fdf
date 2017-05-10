d05ce88bf76be4dba301c04b27672bf66c0f5e0d
Steven TOUPIN <stoupin@e2r7p7.42.fr>
2017-05-10 13:52:42 +0200

    Included better get_next_line

--- main.c
+++ main.c
@@ -1,28 +1,31 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: stoupin <marvin@42.fr>                     +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/09 14:52:22 by stoupin           #+#    #+#             */
-/*   Updated: 2017/05/09 14:52:23 by stoupin          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "get_next_line.h"
 
-#include <unistd.h>
-#include "fdf.h"
-
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	t_env	env;
-	int		err;
+	char	*line;
+	int		fd;
+	int		i;
 
-	(void)argc;
-	(void)argv;
-	err = init(&env);
-	if (err)
-		write(1, "Error\n", 6);
-	die(&env);
+	if (argc != 2)
+	{
+		printf("Usage: %s filename\n", argv[0]);
+		return (0);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		printf("Error opening '%s'.\n", argv[1]);
+		return (0);
+	}
+	i = 0;
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%03d %s\n", i + 1, line);
+		free(line);
+		i++;
+	}
 	return (0);
 }
